fa1d1349ea19bda121c9062fc81c9d15fa48d919
Gabriel Victor <68170206+gavictor@users.noreply.github.com>
2022-04-03 00:51:43 -0300

    Exercício 002
    O segundo exercício do curso, onde soma-se dois números e aprende-se o uso de f-strings.

---
+++ Somando Dois Números.py
@@ -0,0 +1,7 @@
+print('===== Desafio 003 =====')
+print('vamos somaaar')
+num1 = int(input('Digite um valor: '))
+num2 = int(input('Digite outro: '))
+s = num1 + num2
+print(f'a soma entre {num1} e {num2} totaliza em: {s}')
+# a forma antiga de fazer isso era; .format(num1, num2, s)
