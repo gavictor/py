2b54fc3a508ca4cf740fb8f63f43ab85ac1b2407
Gabriel Victor <68170206+gavictor@users.noreply.github.com>
2022-04-03 21:11:37 -0300

    Delete Dobro, Triplo e Raiz Quadrada.py

--- Dobro, Triplo e Raiz Quadrada.py
+++
@@ -1,6 +0,0 @@
-print('===== DESAFIO 006 =====')
-num1 = int(input('Digite um valor: '))
-pot = num1*2
-pot2 = num1*3
-raiz = num1**(1/2)
-print(f'o dobro de {num1} é {pot}\n o triplo, {pot2}\n e a raiz quadrada:{raiz:.2f}')
